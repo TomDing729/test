6cffeea8e82a37d0c0e25d2109fe798074319b03
TomDing729 <327523994@qq.com>
2017-12-25 13:13:43 +0800

    初始化
    初始化

---
+++ src/main/java/com/ziding/code/ZidingApplication.java
@@ -0,0 +1,12 @@
+package com.ziding.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZidingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ziding/code/ZidingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ziding.code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZidingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
